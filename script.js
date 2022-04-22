337ba07ce6210e17e08603e0961582b152420b17
Mark <markwilkins86@gmail.com>
2022-04-22 11:58:33 -0400

    add style.css, add script.js, connect index.html

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ function rollDice() {
     var die5 = document.getElementById("die5");
     var die6 = document.getElementById("die6");
     var status = document.getElementById("status");
+    var triangle = document.getElementById("triangle")
     var d1 = Math.floor(Math.random() * 6) + 1;
     var d2 = Math.floor(Math.random() * 6) + 1;
     var d3 = Math.floor(Math.random() * 6) + 1;
     var d4 = Math.floor(Math.random() * 6) + 1;
     var d5 = Math.floor(Math.random() * 6) + 1;
     var d6 = Math.floor(Math.random() * 6) + 1;
-    var diceTotal = d1 + d2 + d3 + d4 + d5 + d6;
+    var tri = Math.floor(Math.random() * 4) + 1;
+    
+    triangle.innerHTML = tri;
     die1.innerHTML = d1;
     die2.innerHTML = d2;
     die3.innerHTML = d3;
     die4.innerHTML = d4;
     die5.innerHTML = d5;
     die6.innerHTML = d6;
-    status.innerHTML = "You rolled "+diceTotal+".";
+
+    var diceTotal = d1 + d2 + d3 + d4 + d5 + d6;
+    
 }
