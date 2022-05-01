403bdc7d8d002770fd92ab802cbc46f7f2edc32f
Mark <markwilkins86@gmail.com>
2022-05-01 14:45:05 -0400

    add style.css, add script.js, connect index.html

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function rollDice() {
     var die5 = document.getElementById("die5");
     var die6 = document.getElementById("die6");
     var status = document.getElementById("status");
-    var triangle = document.getElementById("triangle")
+   
     var d1 = Math.floor(Math.random() * 6) + 1;
     var d2 = Math.floor(Math.random() * 6) + 1;
     var d3 = Math.floor(Math.random() * 6) + 1;
     var d4 = Math.floor(Math.random() * 6) + 1;
     var d5 = Math.floor(Math.random() * 6) + 1;
     var d6 = Math.floor(Math.random() * 6) + 1;
-    var tri = Math.floor(Math.random() * 4) + 1;
     
-    triangle.innerHTML = tri;
+    
+    
     die1.innerHTML = d1;
     die2.innerHTML = d2;
     die3.innerHTML = d3;
@@ -26,3 +26,26 @@ function rollDice() {
     var diceTotal = d1 + d2 + d3 + d4 + d5 + d6;
     
 }
+
+
+const classes = {
+    '1' : 'Artificer',
+    '2' : 'Barbarian',
+    '3' : 'Bard',
+    '4' : 'Cleric',
+    '5' : 'Druid',
+    '6' : 'Fighter',
+    '7' : 'Monk',
+    '8' : 'Paladin',
+    '9' : 'Ranger',
+    '10': 'Rogue',
+    '11': 'Sorcerer',
+    '12': 'Warlock',
+    '13': 'Wizard',
+}
+
+function randomClass() {
+    const num1 = Math.floor(Math.random()* (13) + 1);
+ return `You will be playing a ${classes}`;
+}
+console.log(randomClass)
